859f57a6c87880c2de574ba1bf94314e024efbee
Brad Beltowski <bbelowski@fullsail.com>
2017-10-03 16:14:25 -0400

    Added Firebase config

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyA7Xrew8kpScS6uxNUcQAivrPDnoXc8Ajk',
+      authDomain: 'harambe-3266e.firebaseapp.com',
+      databaseURL: 'https://harambe-3266e.firebaseio.com',
+      projectId: 'harambe-3266e',
+      storageBucket: 'harambe-3266e.appspot.com',
+      messagingSenderId: '775519206694',
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text>Welcom to my CRM!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
