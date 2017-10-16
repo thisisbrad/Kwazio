6e582abdf6e5218fd8405439b0f1b5b6e63e2c3a
Brad Beltowski <bradbeltowski@gmail.com>
2017-10-16 00:14:49 -0400

    Loaded Fonts

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Expo from 'expo';
 import { Container, Header, Content, Button, Text } from 'native-base';
 import firebase from 'firebase';
 import Login from './components/Login';
 
 export default class App extends React.Component {
-  componentWillMount() {
+  async componentDidMount() {
     const config = {
       apiKey: 'AIzaSyA7Xrew8kpScS6uxNUcQAivrPDnoXc8Ajk',
       authDomain: 'harambe-3266e.firebaseapp.com',
@@ -14,6 +15,11 @@ export default class App extends React.Component {
       messagingSenderId: '775519206694'
     };
     firebase.initializeApp(config);
+
+    await Expo.Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf')
+    });
   }
   render() {
     return (
